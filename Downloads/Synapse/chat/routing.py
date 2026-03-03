cf2781df021dc4d16509da91acad8fc2e5c72e3f
Vigneshwaran M <1972banumathi@gmail.com>
2026-03-03 15:21:11 +0530

    Delete Downloads/Synapse directory

--- Downloads/Synapse/chat/routing.py
+++
@@ -1,8 +0,0 @@
-"""WebSocket URL routing for Synapse chat."""
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<session_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/chat/$',                      consumers.ChatConsumer.as_asgi()),
-]
